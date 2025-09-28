8ceb6537819a35de2cc7ac39a0a74a5ea7f85575
David Lazic <lazic.ddavid@gmail.com>
2025-09-28 20:39:24 +0200

    added verylay

--- library.js
+++ library.js
@@ -6,21 +6,13 @@ const closeBtn = document.querySelector(".modal-close");
 function openModal() {
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
-  document.body.classList.add("no-scroll");
 }
 
 function closeModal() {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
-  document.body.classList.remove("no-scroll");
 }
 
 btnNewBook.addEventListener("click", openModal);
 closeBtn.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
-
-document.addEventListener("keydown", (e) => {
-  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
-    closeModal();
-  }
-});
