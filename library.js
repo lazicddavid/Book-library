87afef54e4a6561f97ce6193ab83d6b5c731b0b1
David Lazic <lazic.ddavid@gmail.com>
2025-09-30 18:46:17 +0200

    added padding

--- library.js
+++ library.js
@@ -1,26 +1,3 @@
-/*const btnNewBook = document.getElementById("btnNewBook");
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const closeBtn = document.querySelector(".modal-close");
-
-const bookForm = document.getElementById("bookForm");
-const inTitle = document.get;
-
-function openModal() {
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hi dden");
-}
-
-function closeModal() {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-}
-
-btnNewBook.addEventListener("click", openModal);
-closeBtn.addEventListener("click", closeModal);
-overlay.addEventListener("click", closeModal);
-*/
-
 document.addEventListener("DOMContentLoaded", () => {
   const btnNewBook = document.getElementById("btnNewBook");
   const modal = document.querySelector(".modal");
